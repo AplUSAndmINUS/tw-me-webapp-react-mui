6c4eb2a10654388562a17e359a9928e43131c02b
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-04-18 21:09:48 -0600

    fixed App page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Welcome to the app!</header>
+      <header className="App-header">
+        <Typography variant="h1">Hello!</Typography>
+      </header>
     </div>
   );
 }
