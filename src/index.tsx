4aff78651c81a547189749e1c0bc7bfdce626a3e
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-04-29 20:50:21 -0600

    added redux store and provider to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+
+/* Custom variables */
+import { store } from './store/store';
 
 /* Pages */
 import About from './pages/about/about';
@@ -26,25 +30,27 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Routes>
-          <Route path="/" element={<PageShell />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="/content" element={<ContentShell />}>
-              <Route path="blog" element={<Blog />} />
-              <Route path="events" element={<Events />} />
-              <Route path="github" element={<GitHub />} />
-              <Route path="music" element={<Music />} />
-              <Route path="portfolio" element={<Portfolio />} />
-              <Route path="writing" element={<Writing />} />
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<PageShell />}>
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="/content" element={<ContentShell />}>
+                <Route path="blog" element={<Blog />} />
+                <Route path="events" element={<Events />} />
+                <Route path="github" element={<GitHub />} />
+                <Route path="music" element={<Music />} />
+                <Route path="portfolio" element={<Portfolio />} />
+                <Route path="writing" element={<Writing />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
             </Route>
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
+          </Routes>
+        </ThemeProvider>
+        </BrowserRouter>
+      </Provider>
   </React.StrictMode>
 );
