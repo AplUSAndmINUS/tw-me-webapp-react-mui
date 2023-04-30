d800ac4f6fc8dca69f6741548e27117b7fb08215
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-04-29 21:54:15 -0600

    added Redux connector store for Dark and Light Mode toggle

--- src/pages/page-shell/page-shell.tsx
+++ src/pages/page-shell/page-shell.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { IconButton, Typography } from '@mui/material';
-import { DarkMode } from '@mui/icons-material';
+import { DarkMode, LightMode } from '@mui/icons-material';
+
+import { toggleTheme } from '../../store/reducers/theme-slice';
 
 export const PageShell = () => {
+  const darkMode = useSelector((state: any) => state.theme.darkMode);
+  const dispatch = useDispatch();
+
   return (
     <>
       <IconButton 
         color="secondary" 
         disableRipple
-        disableFocusRipple>
-        <DarkMode />
+        disableFocusRipple
+        onClick={() => dispatch(toggleTheme())}>
+        {darkMode ? <DarkMode /> : <LightMode />}
       </IconButton>
       <Typography variant="h1">PageShell page</Typography>
     </>
