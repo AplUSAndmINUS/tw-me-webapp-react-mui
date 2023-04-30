8ea11c0da5c1ab7cc6b5affa43aca41081a71749
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-04-29 20:58:59 -0600

    added toggleTheme reducer to the Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import themeSlice from './reducers/theme-slice';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    theme: themeSlice
+  },
 });
 
 export default store;
