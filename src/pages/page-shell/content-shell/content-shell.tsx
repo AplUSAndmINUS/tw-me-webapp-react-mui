3bbc0dc6975c8e04e205078bd39f231664a23b21
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-08-26 14:12:46 -0600

    added content type grid system

--- src/pages/page-shell/content-shell/content-shell.tsx
+++ src/pages/page-shell/content-shell/content-shell.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 import './styled';
 
-export const ContentShell = () => <Typography variant="h1">Content Shell page</Typography>;
+export const ContentShell = () => {
+  const [isHomePage, setIsHomePage] = React.useState(true);
+  return (
+    <Grid container padding={4} spacing={3}>
+      <Grid item xs={12} md={isHomePage ? 6 : 4} alignItems="center" justifyContent="center">
+        <Typography variant="h3">Left Column</Typography>
+      </Grid>
+      <Grid item xs={12} md={isHomePage ? 6 : 8} alignItems="center" justifyContent="center">
+        <Typography variant="h3">Right Column</Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+// export const ContentShell = () => <Typography variant="h1">Content Shell page</Typography>;
 
 export default ContentShell;
